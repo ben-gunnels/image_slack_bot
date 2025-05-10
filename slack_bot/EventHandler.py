589f93167384c3249afe048fb4dd517e2501b26d
miltiades-the-general <miltiadesgeneral@gmail.com>
2025-05-09 17:46:17 -0700

    Restructured for modularity. Added documentation text

--- slack_bot/EventHandler.py
+++ slack_bot/EventHandler.py
@@ -76,6 +76,13 @@ class EventHandler:
             self._handle_files_shared()
 
     def _handle_app_mention(self):
+        """
+            Main entry when the bot is mentioned in the message. 
+            It facilitates the functionality that the user is seeking.
+            Sends a help message if the user solicits help.
+            Initiates the process for file download and image generation.
+            If no file is submitted it invokes the direct prompt image generator.
+        """
         if self.help: # If the help flag is present
             message = messages.HelpMessage(self.user)
             send_message(self.channel_id, message)
@@ -142,10 +149,15 @@ class EventHandler:
         self._generate_image_and_send(output_filename)
 
     def _get_file_from_user(self, file, ext):
+        """
+            Function handles trying to download the file that a user attached to the message.
+            As a side effect it generates the input filename for use later. 
+        """
         # Name the file that will be saved from the User's message
         now = datetime.datetime.now()
         self.input_filename = f"user_submitted_files/{now.strftime('%Y-%m-%d-%H-%M-%S')}.{ext}"
 
+        # From slack helper
         download_slack_file(file["url_private"], self.input_filename)
         if self.verbose:
             send_message(self.channel_id, messages.Download)
@@ -165,41 +177,22 @@ class EventHandler:
 
         
     def _handle_image_prompt_and_generation(self, output_filename, mode="image-edit"):
+        """"
+            Generates the image prompt and the generation of an Ai generated image.
+            It handles the cases of prompt-only and image-edit.
+                prompt-only: No image has been uploaded to the message. The generator will use the client.image.create method to
+                    try to create an an image based on the text body given by the sender.
+                image-edit: An image has been uploaded to the message. The generator will use the client.image.edit method
+                    to try to edit the given image and return a suitable design.
+        """
         try:
-            if self.inject:
-                # Inject the clean text into the prompt to help add instructions.
-                self.text = clean_text(self.text)
-            
-            # Get the dense prompt
-            if mode == "prompt-only":
-                # This will only run if inject is true as it's being handled in the parent function
-                generated_prompt = generate_prompt(mode="prompt-only", injection=self.text)
-                generated_prompt += " Ensure the image has a transparent background."
-            
-            if mode == "image-edit":
-                # Just return the boilerplate prompt
-                if self.inject:
-                    generated_prompt = generate_prompt(mode="image-edit") + self.text
-                else:
-                    generated_prompt = generate_prompt(mode="image-edit")
-                
-            self.logger.info("Prompt generated")
-            if self.verbose:
-                send_message(self.channel_id, messages.PromptGenerated)
-                send_message(self.channel_id, generated_prompt)
+            generated_prompt = self._generate_prompt(mode)
 
-            # Make a call to OpenAi image generation model based on the prompt
-            if mode == "prompt-only":
-                generated_image = generate_image(self.logger, generated_prompt)
-
-            if mode == "image-edit":
-                generated_image = edit_image(self.logger, generated_prompt, self.input_filename)
-
-            if self.verbose: 
-                send_message(self.channel_id, messages.ImageGenerated)
+            generated_image = self._generate_image(mode, generated_prompt)
 
             # Reformat the image to proper dimensions and specs
             generated_image = resize_image(generated_image)
+            
             self.logger.info("Image Resized")
             if self.verbose:
                 send_message(self.channel_id, messages.ImageResized)
@@ -208,11 +201,56 @@ class EventHandler:
             if self.verbose:
                 send_message(self.channel_id, messages.TrySending)
             self.logger.info(f"Generated image saved to {output_filename}")
+
             return 200
+        
         except Exception:
+            send_message(self.channel_id, messages.GeneratorError)
             print(f"Image generation could not be completed.")
 
+    def _generate_prompt(self, mode):
+        if self.inject:
+                # Inject the clean text into the prompt to help add instructions.
+                self.text = clean_text(self.text)
+            
+        # Get the dense prompt
+        if mode == "prompt-only":
+            # This will only run if inject is true as it's being handled in the parent function
+            generated_prompt = generate_prompt(mode="prompt-only", injection=self.text)
+            generated_prompt += " Ensure the image has a transparent background."
+        
+        if mode == "image-edit":
+            # Just return the boilerplate prompt
+            if self.inject:
+                generated_prompt = generate_prompt(mode="image-edit") + self.text
+            else:
+                generated_prompt = generate_prompt(mode="image-edit")
+
+        self.logger.info("Prompt generated")
+        if self.verbose:
+            send_message(self.channel_id, messages.PromptGenerated)
+            send_message(self.channel_id, generated_prompt)
+        
+        return generated_prompt
+    
+    def _generate_image(self, mode, generated_prompt):
+        # Make a call to OpenAi image generation model based on the prompt
+        if mode == "prompt-only":
+            generated_image = generate_image(self.logger, generated_prompt)
+
+        if mode == "image-edit":
+            generated_image = edit_image(self.logger, generated_prompt, self.input_filename)
+
+        if self.verbose: 
+            send_message(self.channel_id, messages.ImageGenerated)
+        
+        return generated_image
+
     def _handle_image_reformatting(self, output_filename):
+        """
+            Function is called when the --reformat flag is invoked.
+            It more directly and succinctly downloads the submitted image and reformats it to the correct size.
+        """
         # Only handle tasks related to reformating the image submitted.
         with open(self.input_filename, "rb") as f:
             image_bytes = f.read()
@@ -232,6 +270,9 @@ class EventHandler:
         send_file(self.channel_id, output_filename, "Here's your reformatted image!")
 
     def _mkdirs(self, folder_path):
+        """
+            Initializes the necessary folders used for image saving and generation.
+        """
         # Check if the folder exists
         if not os.path.exists(folder_path):
             os.makedirs(folder_path)
@@ -240,6 +281,10 @@ class EventHandler:
             self.logger.info(f"Directory already exists: {folder_path}")
 
     def _set_flags(self):
+        """
+            Initializes the flag properties for the object. 
+            Finds the flags in the message which are prepended by --.
+        """
         self.flags = find_flags(self.text)
 
         for flag in VALID_FLAGS:
@@ -247,6 +292,10 @@ class EventHandler:
                 setattr(self, flag, True) 
 
     def _cleanup(self, output_filename):
+        """
+            Removes the images that have been saved locally and temporarily.
+            Removes the input image files and the generated output files.
+        """
         # Remove stored slack image
         if os.path.exists(self.input_filename):
             os.remove(self.input_filename)
